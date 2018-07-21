90a8bd73b77c1d13ce64a14078e68dd43df0e6a4
Lillian N <ngo.lilli94@gmail.com>
2018-07-20 22:34:49 -0400

    Linked images to modal

---
+++ main.js
@@ -0,0 +1,7 @@
+//Defining variables to push Information into modals
+let showName = document.getElementById('displayName')
+let showImg = document.getElementById('displayImage')
+let showAbilities = document.getElementById('displayAbilities')
+let showHP = document.getElementById('displayHP')
+let showAtk = document.getElementById('displayAtk')
+let showDef = document.getElementById('displayDef')
